e061c013b4deca536da30e62a004e7c7691bbb33
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-07-17 15:26:35 +0800

    add basic header

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,19 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: "#000000",
+          secondary: "#000000",
+          accent: "#2f4da1",
+          neutral: "#000000",
+          "base-200": "#1d232a",
+          "base-100": "#13171c",
+          "base-content": "#f3f4f6",
+        },
+      },
+    ],
+  },
 };
