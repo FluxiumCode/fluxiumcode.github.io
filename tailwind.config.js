5194b036dc0146bbad1f221695784f6c77ee33f5
kyteidev <89528285+kyteidev@users.noreply.github.com>
2024-07-18 11:53:55 +0800

    increase mobile support, made logo clickable

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
           primary: "#000000",
           secondary: "#000000",
           accent: "#2f4da1",
+          info: "#4a7aff",
           neutral: "#000000",
           "base-200": "#1d232a",
           "base-100": "#13171c",
